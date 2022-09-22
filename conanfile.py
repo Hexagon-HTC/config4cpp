178a73b62de380f56c73963d182037ad84528f26
lukasz karlowski <lukasz.karlowski-ext@hexagon.com>
2022-09-23 01:32:38 +0200

    Support conan package system

---
+++ conanfile.py
@@ -0,0 +1,41 @@
+from conans import ConanFile, CMake, tools
+
+
+class Config4cppConanConan(ConanFile):
+    name = "config4cppConan"
+    version = "1.0"
+    license = "MIT"
+    description = "<Description of Config4cppConan here>"
+    topics = ("<Put some tag here>", "<here>", "<and here>")
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+    generators = "make"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def source(self):
+        self.run("git clone https://github.com/conan-io/hello.git")
+        # This small hack might be useful to guarantee proper /MT /MD linkage
+        # in MSVC if the packaged project doesn't have variables to set it
+        # properly
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+        # Explicit way:
+        # self.run('cmake %s/hello %s'
+        #          % (self.source_folder, cmake.command_line))
+        # self.run("cmake --build . %s" % cmake.build_config)
+
+    def package(self):
+        self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["config4cppConan"]
+
